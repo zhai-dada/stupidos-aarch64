e9b7b05ff501167ce7a4f785ca7c2a9cec0c8c88
zhai-dada <zhaipengwei0728@gmail.com>
2026-03-13 15:58:29 +0800

    增加arm64 pagetable相关属性 增加相关函数以及宏定义

--- include/asm/pagetable.h
+++ include/asm/pagetable.h
@@ -2,6 +2,7 @@
 #define __PAGETABLE_H__
 
 #include "asm/pagetable_types.h"
+#include "asm/pagetable_port.h"
 #include "asm/barrier.h"
 
 #define pgd_none(pgd) (!pgd_val(pgd))
@@ -9,17 +10,82 @@
 #define pmd_none(pmd) (!pmd_val(pmd))
 #define pte_none(ptd) (!pte_val(ptd))
 
-#define MAIR_DEVICE_nGnRnE_ATTR     0x00
-#define MAIR_DEVICE_nGnRE_ATTR      0x04
-#define MAIR_DEVICE_nGRE_ATTR       0x08
-#define MAIR_DEVICE_GRE_ATTR        0x0c
-// undefined
-#define MAIR_UNDEFINED              0x03
-// 关闭高速缓存
-#define MAIR_NORMAL_NC_ATTR         0x44
-// 写直通策略
-#define MAIR_NORMAL_WT_ATTR         0xbb
-// 回写策略
-#define MAIR_NORMAL_ATTR            0xff
+/* 查找PGD PUD PMD PTE索引 */
+#define pgd_index(addr)                 (((addr) >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1))
+#define pud_index(addr) 				(((addr) >> PUD_SHIFT) & (PTRS_PER_PUD - 1))
+#define pmd_index(addr) 				(((addr) >> PMD_SHIFT) & (PTRS_PER_PMD - 1))
+#define pte_index(addr) 				(((addr) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1))
+
+/* 通过地址addr查找PGD的表项 */
+#define pgd_offset_raw(pgdir, addr)     ((pgdir) + pgd_index(addr))
+
+#define pgd_addr_end(addr, end)									\
+({	uint64_t __boundary = ((addr) + PGDIR_SIZE) & PGDIR_MASK;	\
+	(__boundary - 1 < (end) - 1) ? __boundary : (end);			\
+})
+
+#define pud_addr_end(addr, end)									\
+({	uint64_t __boundary = ((addr) + PUD_SIZE) & PUD_MASK;		\
+	(__boundary - 1 < (end) - 1) ? __boundary : (end);			\
+})
+
+#define pmd_addr_end(addr, end)									\
+({	uint64_t __boundary = ((addr) + PMD_SIZE) & PMD_MASK;		\
+	(__boundary - 1 < (end) - 1) ? __boundary : (end);			\
+})
+
+
+#define pmd_sect(pmd)	((pmd_val(pmd) & PMD_TYPE_MASK) == PMD_TYPE_SECT)
+#define pud_sect(pud)	((pud_val(pud) & PUD_TYPE_MASK) == PUD_TYPE_SECT)
+
+static inline uint64_t pgd_page_paddr(pgd_t pgd)
+{
+	return pgd_val(pgd) & PTE_ADDR_MASK;
+}
+
+#define pud_offset_phys(pgd, addr) ((pud_t *)((pgd_page_paddr(*(pgd)) + pud_index(addr) * sizeof(pud_t))))
+
+static inline uint64_t pud_page_paddr(pud_t pud)
+{
+	return pud_val(pud) & PTE_ADDR_MASK;
+}
+
+#define pmd_offset_phys(pud, addr) ((pmd_t *)(pud_page_paddr(*(pud)) + pmd_index(addr) * sizeof(pmd_t)))
+
+static inline uint64_t pmd_page_paddr(pmd_t pmd)
+{
+	return pmd_val(pmd) & PTE_ADDR_MASK;
+}
+
+#define pte_offset_phys(dir, addr) ((pte_t *)(pmd_page_paddr(*(dir)) + pte_index(addr) * sizeof(pte_t)))
+
+static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
+{
+	*pgdp = pgd;
+
+	dsb(ishst);
+}
+
+static inline void set_pud(pud_t *pudp, pud_t pud)
+{
+	*pudp = pud;
+
+	dsb(ishst);
+}
+
+static inline void set_pmd(pmd_t *pmdp, pmd_t pmd)
+{
+	*pmdp = pmd;
+
+	dsb(ishst);
+}
+
+static inline void set_pte(pte_t *ptep, pte_t pte)
+{
+	*ptep = pte;
+
+	dsb(ishst);
+}
+
 
 #endif
--- kernel/mmu.c
+++ kernel/mmu.c
@@ -1,6 +1,19 @@
 #include "mmu.h"
+#include "asm/types.h"
+#include "debug.h"
 
 void mmu_init(void)
 {
+    uint32_t sctlr_el1;
+
+    asm volatile
+    (
+        "mrs %0, sctlr_el1"
+        : "=r"(sctlr_el1)
+        :
+        : "memory"
+    );
+    
+    printk("sctlr_el1 : %#x\n", sctlr_el1);
     return;
 }
